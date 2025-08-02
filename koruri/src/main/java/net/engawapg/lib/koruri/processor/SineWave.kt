11117c905805cb1d46e55689750271113f67f24f
usuiat <stripewani@gmail.com>
2025-08-02 13:59:08 +0900

    Fix an issue where phase errors cause the frequency of the sine wave to change

--- koruri/src/main/java/net/engawapg/lib/koruri/processor/SineWave.kt
+++ koruri/src/main/java/net/engawapg/lib/koruri/processor/SineWave.kt
@@ -1,6 +1,7 @@
 package net.engawapg.lib.koruri.processor
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import net.engawapg.lib.koruri.audio.Block
 import kotlin.math.PI
 import kotlin.math.sin
@@ -9,6 +10,8 @@ import kotlin.math.sin
 fun SineWave(frequency: Float) {
     Block(signalProcessor = SineWaveGenerator(frequency))
 }
+
+private const val PIx2 = PI.toFloat() * 2.0f
 
 private class SineWaveGenerator(
     private val frequency: Float,
@@ -25,12 +28,16 @@ private class SineWaveGenerator(
             return output
         }
 
-        val phaseDelta = 2.0f * PI.toFloat() * frequency / sampleRate
+        val phaseDelta = PIx2 * frequency / sampleRate
         for (i in 0 until numSamples) {
             val sample = sin(phase) * amp
             output[i * 2] = sample
             output[i * 2 + 1] = sample
             phase += phaseDelta
+            // phase を 2π の範囲内に正規化
+            if (phase >= PIx2) {
+                phase -= PIx2
+            }
         }
 
         return output
