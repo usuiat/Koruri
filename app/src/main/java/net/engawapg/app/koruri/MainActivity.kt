46baed8aa96a38f776b89f9a89e8efbecf1989ef
usuiat <stripewani@gmail.com>
2025-08-03 18:55:26 +0900

    Define melody of Twinkle Twinkle Little Star

--- app/src/main/java/net/engawapg/app/koruri/MainActivity.kt
+++ app/src/main/java/net/engawapg/app/koruri/MainActivity.kt
@@ -47,34 +47,37 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun KoruriSample() {
-    val frequency by produceState(0f) {
-        val C5 = 523.251f // C5 frequency
-        val D5 = 587.330f // D5 frequency
-        val E5 = 659.255f // E5 frequency
-        val F5 = 698.456f // F5 frequency
-        val G5 = 783.991f // G5 frequency
-        val A5 = 880.000f // A5 frequency
-        val B5 = 987.767f // B5 frequency
-
-        val tones = listOf(
-            C5, C5, G5, G5, A5, A5, G5, 0f,
-            F5, F5, E5, E5, D5, D5, C5, 0f,
-            G5, G5, F5, F5, E5, E5, D5, 0f,
-            G5, G5, F5, F5, E5, E5, D5, 0f,
-        )
-        var index = 0
-        while (true) {
-            value = tones[index]
-            delay(500L)
-            value = 0f
-            delay(50L)
-            index = (index + 1) % tones.size
-        }
-    }
+    val frequency by melodyOfTwinkleTwinkleLittleStar()
     FMSynthesis(
         carrierFrequency = frequency,
         modulatorRatio = 4.0f,
         modulationIndex = 1.5f,
     )
+}
+
+@Composable
+private fun melodyOfTwinkleTwinkleLittleStar() = produceState(0f) {
+    val C5 = 523.251f // C5 frequency
+    val D5 = 587.330f // D5 frequency
+    val E5 = 659.255f // E5 frequency
+    val F5 = 698.456f // F5 frequency
+    val G5 = 783.991f // G5 frequency
+    val A5 = 880.000f // A5 frequency
+    val B5 = 987.767f // B5 frequency
+
+    val tones = listOf(
+        C5, C5, G5, G5, A5, A5, G5, 0f,
+        F5, F5, E5, E5, D5, D5, C5, 0f,
+        G5, G5, F5, F5, E5, E5, D5, 0f,
+        G5, G5, F5, F5, E5, E5, D5, 0f,
+    )
+    var index = 0
+    while (true) {
+        value = tones[index]
+        delay(500L)
+        value = 0f
+        delay(50L)
+        index = (index + 1) % tones.size
+    }
 }
 
