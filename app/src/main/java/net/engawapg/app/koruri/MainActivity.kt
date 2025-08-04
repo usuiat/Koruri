81a484650c3198f15547334a83b541a058705b02
usuiat <stripewani@gmail.com>
2025-08-04 22:54:23 +0900

    Define accompaniment

--- app/src/main/java/net/engawapg/app/koruri/MainActivity.kt
+++ app/src/main/java/net/engawapg/app/koruri/MainActivity.kt
@@ -21,7 +21,9 @@ import net.engawapg.app.koruri.ui.theme.KoruriTheme
 import net.engawapg.lib.koruri.KoruriContent
 import net.engawapg.lib.koruri.processor.Instrument
 import net.engawapg.lib.koruri.processor.InstrumentNote
+import net.engawapg.lib.koruri.processor.Mix
 import net.engawapg.lib.koruri.processor.Note
+import net.engawapg.lib.koruri.processor.Pitch.*
 import net.engawapg.lib.koruri.processor.Pitch.A5
 import net.engawapg.lib.koruri.processor.Pitch.C5
 import net.engawapg.lib.koruri.processor.Pitch.D5
@@ -29,6 +31,7 @@ import net.engawapg.lib.koruri.processor.Pitch.E5
 import net.engawapg.lib.koruri.processor.Pitch.F5
 import net.engawapg.lib.koruri.processor.Pitch.G5
 import net.engawapg.lib.koruri.processor.Pitch.Silence
+import net.engawapg.lib.koruri.processor.SineWave
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,8 +64,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun KoruriSample() {
-    val note by melodyOfTwinkleTwinkleLittleStar()
-    InstrumentNote(note = note, instrument = Instrument.Celesta)
+    Mix {
+        val note by melodyOfTwinkleTwinkleLittleStar()
+        InstrumentNote(note = note, instrument = Instrument.Celesta)
+        val accompaniment by accompanimentOfTwinkleTwinkleLittleStar()
+        InstrumentNote(note = accompaniment, instrument = Instrument.Celesta)
+    }
 }
 
 @Composable
@@ -72,6 +79,26 @@ private fun melodyOfTwinkleTwinkleLittleStar() = produceState(Note(Silence)) {
         F5, F5, E5, E5, D5, D5, C5, Silence,
         G5, G5, F5, F5, E5, E5, D5, Silence,
         G5, G5, F5, F5, E5, E5, D5, Silence,
+        C5, C5, G5, G5, A5, A5, G5, Silence,
+        F5, F5, E5, E5, D5, D5, C5, Silence,
+    )
+    var index = 0
+    while (true) {
+        value = Note(pitch[index])
+        delay(500L)
+        index = (index + 1) % pitch.size
+    }
+}
+
+@Composable
+private fun accompanimentOfTwinkleTwinkleLittleStar() = produceState(Note(Silence)) {
+    val pitch = listOf(
+        E4, C4, E4, C4, F4, C4, E4, C4,
+        D4, G4, C4, G4, F4, G4, E4, G4,
+        E4, E4, D4, D4, G4, G4, F4, G4,
+        E4, E4, D4, D4, G4, G4, F4, G4,
+        E4, C4, E4, C4, F4, C4, E4, C4,
+        D4, G4, C4, G4, F4, G4, C4, Silence,
     )
     var index = 0
     while (true) {
